aec6366e2fb5f04ddbe15a3d14f2bada09faad70
Vishv <vishvshroff54@gmail.com>
2020-05-15 22:03:00 +0530

    Initial Commit
    URL Manipulation
    @PATH
    @QUERY
    @QUERYMAP
    @URL

--- app/src/main/java/com/example/roomdatabase/retrofitdemo/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/roomdatabase/retrofitdemo/JsonPlaceHolderApi.java
@@ -1,12 +1,36 @@
 package com.example.roomdatabase.retrofitdemo;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi
 {
     @GET("posts")
-    Call<List<Post>> getPosts();
+    // TODO: 15-05-2020 to pass multiple parameters
+    /*Call<List<Post>> getPosts(
+            @Query("userId") Integer userId1,
+            @Query("userId") Integer userId2,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+            );*/
+    Call<List<Post>> getPosts(@Query("userId") Integer[] userID,
+                              @Query("_sort") String sort,
+                              @Query("_order") String order
+                              );
+
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String,String> parameters);
+
+    @GET("post/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
 }
--- app/src/main/java/com/example/roomdatabase/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/retrofitdemo/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,7 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
-
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +31,26 @@ public class MainActivity extends AppCompatActivity {
                                     .addConverterFactory(GsonConverterFactory.create())
                                     .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+//        getPosts();
+        getComments();
+    }
+
+
+    private void getPosts() {
+        Map<String,String> parameter = new HashMap<>();
+        parameter.put("userId","1");
+        parameter.put("_sort","id");
+        parameter.put("_order","desc");
+
+
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(new Integer[]{2,3,6},"id","desc");
+
+        // TODO: 15-05-2020 Pass parameter as query map
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameter);
+        // TODO: 15-05-2020 if you don't want to pass sort and order by then pass null it will ignored by retrofit
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(4,null,null);
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -59,6 +78,40 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call = jsonPlaceHolderApi
+                .getComments("posts/3/comments");
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful())
+                {
+                    textViewResult.setText("Code : "+response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+                for(Comment comment : comments)
+                {
+                    String content = "";
+                    content+= "ID : " + comment.getId() + "\n";
+                    content+= "Post ID : "+comment.getPostId() + "\n";
+                    content+= "Name : "+comment.getName() + "\n";
+                    content+= "Email : "+comment.getEmail()+"\n";
+                    content+= "Text : "+comment.getText()+"\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
