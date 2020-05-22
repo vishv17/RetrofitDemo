076356ccccaae32511785a78695260983fab9e15
vishv17 <vishvshroff54@gmail.com>
2020-05-22 22:25:32 +0530

    Add Headers to Retrofit Request

--- app/src/main/java/com/example/roomdatabase/retrofitdemo/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/roomdatabase/retrofitdemo/JsonPlaceHolderApi.java
@@ -11,6 +11,9 @@ import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -60,8 +63,11 @@ public interface JsonPlaceHolderApi
      * @param post
      * @return
      */
+    @Headers({"Static-Header: 123","Static-Header2: 456"})
     @PUT("posts/{id}")
-    Call<Post> putPost(@Path("id") int id,@Body Post post);
+    Call<Post> putPost(@Header ("Dynamic-Header") String header ,
+                                    @Path("id") int id,
+                                    @Body Post post);
 
     /**
      * PATCH method will update only passed parameter in database
@@ -70,7 +76,9 @@ public interface JsonPlaceHolderApi
      * @return
      */
     @PATCH("posts/{id}")
-    Call<Post> patchPost(@Path("id") int id,@Body Post post);
+    Call<Post> patchPost(@HeaderMap Map<String,String> headers,
+                                        @Path("id") int id,
+                                        @Body Post post);
 
     @DELETE("posts/{id}")
     Call<Void> deletePost(@Path("id") int id);
--- app/src/main/java/com/example/roomdatabase/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/retrofitdemo/MainActivity.java
@@ -8,11 +8,16 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -40,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
          OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                 .addInterceptor(new Interceptor() {
+                     @NotNull
+                     @Override
+                     public okhttp3.Response intercept(@NotNull Chain chain) throws IOException {
+                         Request originalReq = chain.request();
+
+                         Request newRequest = originalReq.newBuilder()
+                                                                .header("Interceptor-Header","xyz")
+                                                                .build();
+                         return null;
+                     }
+                 })
                  .addInterceptor(httpLoggingInterceptor)
                  .build();
 
@@ -51,10 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-//        getPosts();
+        getPosts();
 //        getComments();
 //        createPost();
-        updatePost();
+//        updatePost();
 //        deletePost();
     }
 
@@ -174,7 +191,11 @@ public class MainActivity extends AppCompatActivity {
     private void updatePost() {
         Post post = new Post(12,null,"New Text");
 
-        Call<Post> call = jsonPlaceHolderApi.putPost(5,post);
+//        Call<Post> call = jsonPlaceHolderApi.putPost("abc",5,post);
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Map-Header1","def");
+        headers.put("Map-Header2","jhi");
+        Call<Post> call = jsonPlaceHolderApi.patchPost(headers,5,post);
         call.enqueue(new Callback<Post>() {
             @Override
             public void onResponse(Call<Post> call, Response<Post> response) {
